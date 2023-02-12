ad43f07bd3fc99e5a3650f270c0d61ed65544020
mamzel63anastasia <mamzel.anastasia@gmail.com>
2023-02-12 19:44:23 +0400

    Добавить пользователя и производителя

---
+++ src/main/java/models/Producer.java
@@ -0,0 +1,52 @@
+package models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+ @Entity
+ @Table( name = "producer", schema = "public")
+public class Producer {
+     @Id
+     @GeneratedValue (generator = "UUID")
+     @GenericGenerator(
+             name =  "UUID",
+             strategy = "org.hibernate.id.UUIDGeneration"
+     )
+     @Column(name = "id", unique = true, updatable = false)
+    private UUID id;
+
+     @Basic
+     @ Column(name = "producer_name", nullable = false)
+    private  String name;
+
+     @Basic
+     @Column(name = "country", nullable =  false)
+    private String country;
+
+
+     public UUID getId() {
+         return id;
+     }
+
+     public void setId(UUID id) {
+         this.id = id;
+     }
+
+     public String getName() {
+         return name;
+     }
+
+     public void setName(String name) {
+         this.name = name;
+     }
+
+     public String getCountry() {
+         return country;
+     }
+
+     public void setCountry(String country) {
+         this.country = country;
+     }
+ }
---
+++ src/main/java/models/User.java
@@ -0,0 +1,65 @@
+package models;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table (name = "users", schema = "public")
+public class User {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", unique = true, updatable =  false, nullable = false)
+    private UUID id;
+
+   @Basic
+   @Column(name = "login", updatable = false, nullable = false)
+    private String login;
+
+    @Basic
+    @Column(name = "email", updatable = false, nullable = false)
+    private String email;
+
+    @Basic
+    @Column(name = " password", nullable = false)
+    private String password;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
